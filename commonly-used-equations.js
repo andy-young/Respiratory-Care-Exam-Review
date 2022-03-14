1a14d1cdc4b92f1d5d6ed2aa52d7b05e9b1ca364
andy-young <ayscript@gmail.com>
2022-03-14 14:07:20 -0600

    equations object methods hollow

---
+++ commonly-used-equations.js
@@ -0,0 +1,56 @@
+
+ const eq = {
+    cylinder() {},
+
+    liquid() {},
+
+    AV_diff() {},
+
+    totalFlow() {},
+
+    HeO2Flow() {},
+
+    RH() {},
+
+    BH() {},
+
+    HD() {},
+
+    PAO2() {},
+
+    AaGradient() {},
+
+    QSQT() {},
+
+    SVR() {},
+
+    PVR() {},
+
+    QT() {},
+
+    IBW() {},
+
+    TubingCompliance() {},
+
+    VTdelivered() {},
+
+    DynamicCompliance() {},
+
+    StaticCompliance() {},
+
+    InspFlowRate() {},
+
+    IEratio() {},
+
+    ITime() {},
+
+    VDVTratio() {},
+
+    desiredFiO2() {},
+
+    desiredVentRate() {},
+
+    desiredVE() {},
+
+    desiredVT() {}
+};
