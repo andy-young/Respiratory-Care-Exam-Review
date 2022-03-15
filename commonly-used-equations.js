c6102e2b3615b650a9de713b33d09eb360144ae0
andy-young <ayscript@gmail.com>
2022-03-15 12:44:21 -0600

    15 of 27 equations

--- commonly-used-equations.js
+++ commonly-used-equations.js
@@ -31,47 +31,103 @@ const eq = {
     
     HeO2Flow() {},
     
-    RH() {},
+    relativeHumidity(absHumidity, capacity) {
+        const rh = (absHumidity / capacity) * 100;  // absHumidity is amount of H2O in a given volume of gas.
+        return `Relative Humidity is ${rh}.`;       // capacity is the total amount of water capable of being held in the gas at a given temperature.
+    },
     
-    BH() {},
+    bodyHumidity(absHumidity) {
+        const bh = (absHumidity / 44) * 100;
+        return `Body Humidity is ${bh}.`;
+    },
     
-    HD() {},
+    humidityDeficit(absHumidity) {
+        const hd = 44 - absHumidity;
+        const hdPercent = hd / 44;
+        return `The Humidity Deficit is ${hd}, or ${hdPercent}%.`;
+    },
     
-    PAO2() {},
+    PAO2(percentO2, PaCO2) {  // Partial Pressure of Aveolar Oxygen
+        const PAO2 = (7 * percentO2) - (PaCO2 + 10);
+        return `The Partial Pressure of Aveolar Oxygen is ${PAO2}.`;
+    },
     
-    AaGradient() {},
+    AaGradient(PAO2, PaO2) { // Alveolar-arterial oxygen pressure difference
+        const aaGrad = PAO2 - PaO2;
+        return `The A-a gradient is ${aaGrad}.`;
+    },
     
-    QSQT() {},
+    QSQT(AaGradient) {
+        const shunt = (AaGradient / 20) + 4 + "%";
+        return `The Pulmonary Shunt is ${shunt}.`;
+    },
     
-    SVR() {},
+    SVR(QT, MSAP, CVP) { // CVP: central venous pressure
+        const SVR = (MSAP - CVP) / QT;
+        return `The systemic vascular resistance is ${SVR} mmHg/L/min.`;
+    },
     
-    PVR() {},
+    PVR(MPAP, PCWP, QT) { // Mean Pulmonary artery pressure & Pulmonary capillary wedge pressure
+        const PVR = (MPAP - CVP /*mmHg*/) / QT;
+        return `The Pulmonary vascular resistance is ${PVR} mmHg/L/min.`;
+    },
     
-    QT() {},
+    QT(VO2, CaO2, CVO2) { // VO2 = Oxygen Consumption, CaO2 = Cardiac content, CVO2 = Mixed Venous content
+        const QT = VO2 / ((CaO2 - CVO2) * 10);
+        return `The Cardiac Output is ${QT}.`;
+    },
     
-    IBW() {},
+    IBW(heightInches) {
+        const female = 105 + 5(heightInches - 60);
+        const male = 106 + 6(heightInches - 60);
+        return `IBW for female is ${female} lbs, and ${male} lbs for men`;
+    },
     
-    TubingCompliance() {},
+    TubingCompliance() {
+        
+    },
     
-    VTdelivered() {},
+    VTdelivered() {
+        
+    },
     
-    DynamicCompliance() {},
+    DynamicCompliance() {
+        
+    },
     
-    StaticCompliance() {},
+    StaticCompliance() {
+        
+    },
     
-    InspFlowRate() {},
+    InspFlowRate() {
+        
+    },
     
-    IEratio() {},
+    IEratio() {
+        
+    },
     
-    ITime() {},
+    ITime() {
+        
+    },
     
-    VDVTratio() {},
+    VDVTratio() {
+        
+    },
     
-    desiredFiO2() {},
+    desiredFiO2() {
+        
+    },
     
-    desiredVentRate() {},
+    desiredVentRate() {
+        
+    },
     
-    desiredVE() {},
+    desiredVE() {
+        
+    },
     
-    desiredVT() {}
+    desiredVT() {
+        
+    }
 };
